b5478138f7987c9a03e8fd153c3854d8888ef6a7
Dmitrii <kuzneczovdv@gmail.com>
2024-02-04 14:43:53 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void PrintNaturalNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNaturalNumbers(m + 1, n);
+        }
+    }
+
+    static void Main()
+    {
+        int m = 1;
+        int n = 10;
+
+        PrintNaturalNumbers(m, n);
+    }
+}
