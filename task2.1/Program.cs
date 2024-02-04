42fed24ddc6c87ad1f1bbfb18a459b481deb33fd
Dmitrii <kuzneczovdv@gmail.com>
2024-02-04 15:00:50 +0300

    task2.1

---
+++ task2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class AckermannFunction
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+        else if (n == 0)
+            return Ackermann(m - 1, 1);
+        else
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+
+    static void Main()
+    {
+        int m = 2;
+        int n = 3;
+        int result = Ackermann(m, n);
+
+        Console.WriteLine($"Ackermann({m}, {n}) = {result}");
+    }
+}
