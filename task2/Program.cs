4975c27a99d61c4bc50e7900f92e0b79bad22c2e
Dmitrii <kuzneczovdv@gmail.com>
2024-02-04 14:55:06 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int M = 1;
+        int N = 10;
+
+        PrintEvenNumbers(M, N);
+    }
+    static void PrintEvenNumbers(int current, int end)
+    {
+        if (current > end)
+            return;
+
+        if (current % 2 == 0)
+            Console.WriteLine(current);
+
+        PrintEvenNumbers(current + 1, end);
+    }
+}
